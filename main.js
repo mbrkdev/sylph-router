7c778a68ba33bf15eeb4137cbe9e0c147298c854
jetdevuk <jetdevuk@gmail.com>
2019-10-10 22:40:23 +0100

    Added tests and first working version

--- main.js
+++ main.js
@@ -1,37 +1,38 @@
 const readdirp = require('readdirp');
 const path = require('path');
 
-function replace(path) {
-  return path
-  .replace(/\\/gi, '/') // Backslash to forward slash
-  .replace(/\/$/gi, '') // Remove ending slash (for xx/index)
-  .replace(/ /gi, '-') // Spaces to dashes
-  .replace('index.js', '.js') // Change index to just .js
-  .replace(/\.js/g, '') // Change index to just .js
-  .replace(/_/gi, ':') // Underscore to colon (for dynamic routes)
-  .replace(/\/$/gi, '') // Remove ending slash (for xx/index)
+function replace(route) {
+  return route
+    .replace(/\\/gi, '/') // Backslash to forward slash
+    .replace(/\/$/gi, '') // Remove ending slash (for xx/index)
+    .replace(/ /gi, '-') // Spaces to dashes
+    .replace('index.js', '.js') // Change index to just .js
+    .replace(/\.js/g, '') // Change index to just .js
+    .replace(/_/gi, ':') // Underscore to colon (for dynamic routes)
+    .replace(/\/$/gi, ''); // Remove ending slash (for xx/index)
 }
 
 async function scan(root, imports, options) {
-  let routes = {}
-  let replaceFunction = options && options.replace || replace;
-  
+  const routes = {};
+  const replaceFunction = options ? options.replace : replace;
+
   const r = await readdirp.promise(root, {
     fileFilter: '*.js',
-    directoryFilter: ['!public', '!*utils'],
+    directoryFilter: options ? options.filter : ['!public', '!*utils'],
   });
-  r.map((route) => {
-    const routePath = replaceFunction(route.path)
-    const _r = {}
-    for (let i = 0; i < imports.length; i++) {
-      const imp = require(path.resolve(root, route.path))[imports[i]]
-      _r[imports[i]] = imp
+  for (let y = 0; y < r.length; y += 1) {
+    const route = r[y];
+    const routePath = replaceFunction(route.path);
+    const routeObject = {};
+    for (let i = 0; i < imports.length; i += 1) {
+      const imp = require(path.resolve(root, route.path))[imports[i]]; //eslint-disable-line
+      routeObject[imports[i]] = imp;
     }
-    routes[routePath] = _r;
-  });
+    routes[routePath] = routeObject;
+  }
   return routes;
 }
 
 module.exports = {
-  scan
-}
+  scan,
+};
