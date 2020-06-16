1b5e9c66681f2a6317ac647c825d295f496b9c21
Matt Barker <jetdevuk@gmail.com>
2020-06-16 01:16:44 +0100

    Formatting! (Thanks to ESLint & Prettier)

--- main.ts
+++ main.ts
@@ -1,50 +1,50 @@
-import {default as readdirp, EntryInfo} from 'readdirp';
+import { default as readdirp, EntryInfo } from 'readdirp';
 import * as path from 'path';
 
 export interface ScanOptions {
-  directoryBanlist: string[]
-  fileFilter?: string[]
+  directoryBanlist: string[];
+  fileFilter?: string[];
 }
 
-const defaultScanOptions:ScanOptions = {
+const defaultScanOptions: ScanOptions = {
   directoryBanlist: [],
-  fileFilter: ['*.js', '*.ts', '!*.test.js', '!*.test.ts']
-}
+  fileFilter: ['*.js', '*.ts', '!*.test.js', '!*.test.ts'],
+};
 
 export interface ScanResults {
-  routes: string[]
+  routes: string[];
 }
 
 export async function scan(root: string, imports: string[], scanOptions?: ScanOptions): Promise<ScanResults> {
   // Resolve root from working dir & apply options over defaults.
   const resolvedRoot = path.resolve(process.cwd(), root);
-  const options = {...defaultScanOptions, ...scanOptions}
-  
+  const options = { ...defaultScanOptions, ...scanOptions };
+
   // Convert '\' to '/' in file paths
   const normalisedBanlist: string[] = [];
-  options.directoryBanlist.forEach(filter => {
-    normalisedBanlist.push(filter.replace('\\', '/'))
+  options.directoryBanlist.forEach((filter) => {
+    normalisedBanlist.push(filter.replace('\\', '/'));
   });
 
   const files = await readdirp.promise(resolvedRoot, {
     fileFilter: options.fileFilter,
-        
-    directoryFilter: (entry: EntryInfo):boolean => {
-      for (let i = 0; i < normalisedBanlist.length; i+=1) {
+
+    directoryFilter: (entry: EntryInfo): boolean => {
+      for (let i = 0; i < normalisedBanlist.length; i += 1) {
         const filter = normalisedBanlist[i];
-        const normalisedPath = entry.path.replace('\\', '/')
+        const normalisedPath = entry.path.replace('\\', '/');
         if (normalisedPath === filter) return false;
       }
-      return true
-    }
+      return true;
+    },
   });
   console.log('------------');
-  const _r:string[] = [];
-  files.forEach(file => {
+  const _r: string[] = [];
+  files.forEach((file) => {
     console.log(` - ${file.path}`);
-    _r.push(file.path)
-  })
+    _r.push(file.path);
+  });
   console.log(_r);
-  
-  return {routes:_r};
+
+  return { routes: _r };
 }
